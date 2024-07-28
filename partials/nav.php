87980d268dfede78e2a758baf9f5402fcaaf3517
jordand2003 <jed56@njit.edu>
2024-07-28 19:16:06 -0400

    adding user associations page for admin

--- partials/nav.php
+++ partials/nav.php
@@ -44,6 +44,7 @@ session_start();
             <li><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
             <li><a href="<?php echo get_url('admin/data_creation.php'); ?>">Add Product</a></li>
             <li><a href="<?php echo get_url('admin/fetch_api_data.php'); ?>">Fetch API Data</a></li>
+            <li><a href="<?php echo get_url('admin/all_user_associations.php'); ?>">All User Associations</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('list_data.php'); ?>">View Products</a></li>
